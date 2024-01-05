7d905f1ecdce844b77842b29c41cd3d8a34a553d
Ben McKillop <ben.mckillop@digital.cabinet-office.gov.uk>
2024-01-05 14:24:06 +0000

    feat: Initial work on refactor

---
+++ SecureStore-Demo/SecureStore-Demo/SecureStore_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureStore_DemoApp.swift
+//  SecureStore-Demo
+//
+//  Created by McKillop, Ben on 04/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SecureStore_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
