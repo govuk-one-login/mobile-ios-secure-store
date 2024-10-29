ae08be8c18ef6dbc36917c1f12d602fbfc16ae45
dylanjames-dd <dylan.james@digital.cabinet-office.gov.uk>
2024-10-29 16:29:25 +0000

    refactor: descriptive properties on JWK struct
    kept to spec by coding keys
    
    Co-authored-by: Oliver Binns <obinns@deloitte.co.uk>

--- Sources/CryptoService/DataTypes/JWKs.swift
+++ Sources/CryptoService/DataTypes/JWKs.swift
@@ -3,9 +3,24 @@ struct JWKs: Encodable {
 }
 
 struct JWK: Encodable {
-    let kty = "EC"
-    let use = "sig"
-    let crv = "P-256"
+    let keyType = "EC"
+    let intendedUse: IntendedUse = .signing
+    let ellipticCurve: EllipticCurve
     let x: String
     let y: String
+    
+    enum CodingKeys: String, CodingKey {
+        case x, y
+        case keyType = "kty"
+        case intendedUse = "use"
+        case ellipticCurve = "cry"
+    }
+    
+    enum IntendedUse: String, Encodable {
+        case signing = "sig"
+    }
+    
+    enum EllipticCurve: String, Encodable {
+        case primeField256Bit = "P-256"
+    }
 }
