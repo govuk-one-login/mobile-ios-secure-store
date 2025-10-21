f06dbb8c78b7ddab8dd1f6132b50c6b740177f5f
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2025-10-21 16:43:14 +0100

    chore: working on KeyManagerService

--- Sources/SecureStore/KeyManagerService.swift
+++ Sources/SecureStore/KeyManagerService.swift
@@ -8,19 +8,21 @@ final class KeyManagerService {
         self.configuration = configuration
         
         do {
-            try createKeysIfNeeded(name: configuration.id)
+            try createKeysIfNeeded()
         } catch {
             return
         }
     }
+    
+    private lazy var privateKeyIdentifier = { configuration.id + "PrivateKey" }()
+    private lazy var publicKeyIdentifier = { configuration.id + "PublicKey" }()
 }
 
 // MARK: Interaction with SecureEnclave
 extension KeyManagerService {
     // Creating a key pair where the public key is stored in the keychain
     // and the private key is stored in the Secure Enclave
-    func createKeysIfNeeded(name: String) throws {
-        
+    func createKeysIfNeeded() throws {
         // Check if keys already exist in storage
         do {
             _ = try retrieveKeys()
@@ -35,11 +37,21 @@ extension KeyManagerService {
         let requirement = configuration.accessControlLevel.flags
         #endif
         
-        guard let access = SecAccessControlCreateWithFlags(kCFAllocatorDefault,
-                                                           kSecAttrAccessibleWhenUnlockedThisDeviceOnly,
-                                                           requirement,
-                                                           nil),
-              let tag = name.data(using: .utf8) else { return }
+        // Delete keys if none were found to avoid errors around multiple keys
+        try deleteKeys()
+        
+        var flagError: Unmanaged<CFError>?
+        guard let access = SecAccessControlCreateWithFlags(
+            kCFAllocatorDefault,
+            kSecAttrAccessibleWhenUnlockedThisDeviceOnly,
+            requirement,
+            &flagError
+        ) else {
+            guard let error = flagError?.takeRetainedValue() as? Error else {
+                throw SecureStoreError.cantEncryptData
+            }
+            throw error
+        }
         
         let attributes: NSDictionary = [
             kSecAttrKeyType: kSecAttrKeyTypeECSECPrimeRandom,
@@ -47,13 +59,16 @@ extension KeyManagerService {
             kSecAttrTokenID: kSecAttrTokenIDSecureEnclave,
             kSecPrivateKeyAttrs: [
                 kSecAttrIsPermanent: true,
-                kSecAttrApplicationTag: tag,
+                kSecAttrApplicationTag: Data(configuration.id.utf8),
                 kSecAttrAccessControl: access
             ]
         ]
         
         var error: Unmanaged<CFError>?
-        guard let privateKey = SecKeyCreateRandomKey(attributes, &error) else {
+        guard let privateKey = SecKeyCreateRandomKey(
+            attributes,
+            &error
+        ) else {
             guard let error = error?.takeRetainedValue() as? Error else {
                 throw SecureStoreError.cantEncryptData
             }
@@ -64,36 +79,49 @@ extension KeyManagerService {
             throw SecureStoreError.cantGetPublicKeyFromPrivateKey
         }
         
-        try storeKeys(keyToStore: publicKey, name: "\(name)PublicKey")
-        try storeKeys(keyToStore: privateKey, name: "\(name)PrivateKey")
+        try storeKeys(
+            keyToStore: privateKey,
+            name: privateKeyIdentifier
+        )
+        try storeKeys(
+            keyToStore: publicKey,
+            name: publicKeyIdentifier
+        )
     }
     
     // Store a given key to the keychain in order to reuse it later
-    func storeKeys(keyToStore: SecKey, name: String) throws {
-        let key = keyToStore
-        let tag = name.data(using: .utf8)!
-        let addquery: [String: Any] = [kSecClass as String: kSecClassKey,
-                                       kSecAttrApplicationTag as String: tag,
-                                       kSecValueRef as String: key]
+    func storeKeys(
+        keyToStore: SecKey,
+        name: String
+    ) throws {
+        let addQuery: NSDictionary = [
+            kSecClass: kSecClassKey,
+            kSecAttrApplicationTag: Data(name.utf8),
+            kSecValueRef: keyToStore
+        ]
         
         // Add item to KeyChain
-        let status = SecItemAdd(addquery as CFDictionary, nil)
-        guard status == errSecSuccess else {
+        guard SecItemAdd(
+            addQuery as CFDictionary,
+            nil
+        ) == errSecSuccess else {
             throw SecureStoreError.cantStoreKey
         }
     }
     
     // Deletes a given key to the keychain
     func deleteKeys() throws {
-        let keyType = ["PublicKey", "PrivateKey"]
-        try keyType.forEach { key in
-            let keyName = configuration.id + key
-            let tag = keyName.data(using: .utf8)!
-            let addquery: [String: Any] = [kSecClass as String: kSecClassKey,
-                                           kSecAttrApplicationTag as String: tag]
+        try [
+            privateKeyIdentifier,
+            publicKeyIdentifier
+        ].forEach { key in
+            let deleteQuery: NSDictionary = [
+                kSecClass: kSecClassKey,
+                kSecAttrApplicationTag: Data(key.utf8)
+            ]
             
-            let status = SecItemDelete(addquery as CFDictionary)
-            guard status == errSecSuccess else {
+            let status = SecItemDelete(deleteQuery as CFDictionary)
+            guard status == errSecSuccess || status == errSecItemNotFound else {
                 throw SecureStoreError.cantDeleteKey
             }
         }
@@ -102,9 +130,6 @@ extension KeyManagerService {
     // Retrieve a key that has been stored before
     func retrieveKeys(localAuthStrings: LocalAuthenticationLocalizedStrings? = nil) throws -> (publicKey: SecKey,
                                                                                                privateKey: SecKey) {
-        let privateKeyTag = Data("\(configuration.id)PrivateKey".utf8)
-        let publicKeyTag = Data("\(configuration.id)PublicKey".utf8)
-        
         // This constructs a query that will be sent to keychain
         var privateQuery: NSDictionary {
             let context = LAContext()
@@ -117,34 +142,36 @@ extension KeyManagerService {
             }
             return [
                 kSecClass: kSecClassKey,
-                kSecAttrApplicationTag: privateKeyTag,
-                kSecUseAuthenticationContext as String: context,
+                kSecAttrApplicationTag: Data(privateKeyIdentifier.utf8),
+                kSecUseAuthenticationContext: context,
                 kSecAttrKeyType: kSecAttrKeyTypeECSECPrimeRandom,
                 kSecReturnRef: true
             ]
         }
         
-        var privateKey: CFTypeRef?
-        let privateStatus = SecItemCopyMatching(privateQuery as CFDictionary, &privateKey)
-        
         // errSecSuccess is the result code returned when no error was found with the query
-        guard privateStatus == errSecSuccess else {
+        var privateKey: CFTypeRef?
+        guard SecItemCopyMatching(
+            privateQuery as CFDictionary,
+            &privateKey
+        ) == errSecSuccess else {
             throw SecureStoreError.cantRetrieveKey
         }
         
         // This constructs a query that will be sent to keychain
         let publicQuery: NSDictionary = [
             kSecClass: kSecClassKey,
-            kSecAttrApplicationTag: publicKeyTag,
+            kSecAttrApplicationTag: Data(publicKeyIdentifier.utf8),
             kSecAttrKeyType: kSecAttrKeyTypeECSECPrimeRandom,
             kSecReturnRef: true
         ]
         
-        var publicKey: CFTypeRef?
-        let publicStatus = SecItemCopyMatching(publicQuery as CFDictionary, &publicKey)
-        
         // errSecSuccess is the result code returned when no error was found with the query
-        guard publicStatus == errSecSuccess else {
+        var publicKey: CFTypeRef?
+        guard SecItemCopyMatching(
+            publicQuery as CFDictionary,
+            &publicKey
+        ) == errSecSuccess else {
             throw SecureStoreError.cantRetrieveKey
         }
         
@@ -157,43 +184,45 @@ extension KeyManagerService {
 // MARK: Encryption and Decryption
 extension KeyManagerService {
     func encryptDataWithPublicKey(dataToEncrypt: String) throws -> String {
-        let publicKey = try retrieveKeys().publicKey
-        
-        guard let formattedData = dataToEncrypt.data(using: String.Encoding.utf8) else {
-            throw SecureStoreError.cantEncodeData
-        }
-        
         var error: Unmanaged<CFError>?
-        guard let encryptData = SecKeyCreateEncryptedData(publicKey,
-                                                          SecKeyAlgorithm.eciesEncryptionStandardX963SHA256AESGCM,
-                                                          formattedData as CFData,
-                                                          &error) else {
-            throw SecureStoreError.biometricErrorHandling(error: error?.takeRetainedValue(), defaultError: SecureStoreError.cantEncryptData)
+        guard let encryptedData = SecKeyCreateEncryptedData(
+            try retrieveKeys().publicKey,
+            SecKeyAlgorithm.eciesEncryptionStandardX963SHA256AESGCM,
+            Data(dataToEncrypt.utf8) as CFData,
+            &error
+        ) else {
+            throw SecureStoreError.biometricErrorHandling(
+                error: error?.takeRetainedValue(),
+                defaultError: SecureStoreError.cantEncryptData
+            )
         }
         
-        let encryptedData = encryptData as Data
-        let encryptedString = encryptedData.base64EncodedString(options: [])
-        
-        return encryptedString
+        return (encryptedData as Data).base64EncodedString()
     }
     
     func decryptDataWithPrivateKey(dataToDecrypt: String) throws -> String {
-        let privateKeyRepresentation = try retrieveKeys(localAuthStrings: configuration.localAuthStrings).privateKey
-        
-        guard let formattedData = Data(base64Encoded: dataToDecrypt, options: [])  else {
+        guard let formattedData = Data(base64Encoded: dataToDecrypt) else {
             throw SecureStoreError.cantFormatData
         }
         
-        var error: Unmanaged<CFError>?
         // Pulls from Secure Enclave - here is where we will look for FaceID/Passcode
-        guard let decryptData = SecKeyCreateDecryptedData(privateKeyRepresentation,
-                                                          SecKeyAlgorithm.eciesEncryptionStandardX963SHA256AESGCM,
-                                                          formattedData as CFData,
-                                                          &error) else {
-            throw SecureStoreError.biometricErrorHandling(error: error?.takeRetainedValue(), defaultError: SecureStoreError.cantDecryptData)
+        var error: Unmanaged<CFError>?
+        guard let decryptedData = SecKeyCreateDecryptedData(
+            try retrieveKeys(localAuthStrings: configuration.localAuthStrings).privateKey,
+            SecKeyAlgorithm.eciesEncryptionStandardX963SHA256AESGCM,
+            formattedData as CFData,
+            &error
+        ) else {
+            throw SecureStoreError.biometricErrorHandling(
+                error: error?.takeRetainedValue(),
+                defaultError: SecureStoreError.cantDecryptData
+            )
         }
         
-        guard let decryptedString = String(data: decryptData as Data, encoding: .utf8) else {
+        guard let decryptedString = String(
+            data: decryptedData as Data,
+            encoding: .utf8
+        ) else {
             throw SecureStoreError.cantDecodeData
         }
         
