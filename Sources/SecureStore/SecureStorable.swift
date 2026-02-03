6308ff5e6fa9ef376094f54882ec5e7759f62d45
shukriahmed-dd <shukri.ahmed@digital.cabinet-office.gov.uk>
2026-02-03 11:21:34 +0000

    chore: update protocol

--- Sources/SecureStore/SecureStorable.swift
+++ Sources/SecureStore/SecureStorable.swift
@@ -2,8 +2,12 @@
 ///
 /// Used for saving items to keychain storage
 public protocol SecureStorable {
+    // TODO: DCMAW-18331 delete function
     func saveItem(item: String, itemName: String) throws
     func readItem(itemName: String) throws -> String
+    
+    func saveItemV2(item: String, itemName: String) throws
+    func readItemV2(itemName: String) throws -> String
     func deleteItem(itemName: String)
     func delete() throws
     func checkItemExists(itemName: String) -> Bool
