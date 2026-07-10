16ae7d03361d902a82d8c46bb8ea338f965e19d5
Markos Zoulias Charatzas <markos.charatzas@dsit.gov.uk>
2026-07-10 14:41:24 +0100

    chore: PR comments

--- Package.swift
+++ Package.swift
@@ -31,7 +31,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/govuk-one-login/mobile-ios-utilities",
-	     .upToNextMajor(from: "1.0.0")
+            .upToNextMajor(from: "1.0.0")
         )
     ],
     targets: [
