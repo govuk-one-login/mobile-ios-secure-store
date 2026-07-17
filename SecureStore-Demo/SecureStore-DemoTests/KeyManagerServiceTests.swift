10910c92d06ce8910351aedb92e8f68cc8886afb
Markos Zoulias Charatzas <Markos.Charatzas@dsit.gov.uk>
2026-07-17 10:30:39 +0100

    feat: DCMAW-21030 increased test coverage (#71)
    Added cases of errSecParam (i.e. -50) error

--- SecureStore-Demo/SecureStore-DemoTests/KeyManagerServiceTests.swift
+++ SecureStore-Demo/SecureStore-DemoTests/KeyManagerServiceTests.swift
@@ -1,4 +1,5 @@
 import Foundation
+import LocalAuthentication
 @testable import SecureStore
 import Testing
 
@@ -63,5 +64,95 @@ struct KeyManagerServiceTests: ~Copyable {
 
         let publicKey = try #require(SecKeyCopyPublicKey(privateKey))
         #expect(keys.publicKey == publicKey)
+    }
+
+    /// This is a case where a as part of instantiating a `KeyManagerService`, a new set of keys is created
+    /// that is tied to the Secure Enclave (i.e. `kSecAttrTokenID: kSecAttrTokenIDSecureEnclave`).
+    ///
+    /// The keys are explicitly deleted by calling `deleteKeys()` in the  `KeyManagerService`
+    ///
+    /// When attempting to **decrypt** the data, an `errSecParam` (aka -50) will be thrown
+    @Test("""
+            GIVEN a new `KeyManagerService` with a set of keys
+            AND a data is encrypted
+            AND the keys are explicitly deleted
+            AND a new `KeyManagerService` is created with the same (i.e. same `id`) configuration
+            WHEN attempting to decrypt the data
+            THEN throws `SecureStoreError.cantDecryptData`
+            AND the `originalError` is `errSecParam` (i.e. -50 OSStatus)
+    """)
+    func decryptKeyWithWrongPrivateKey() async throws {
+
+        let anyString = "any"
+        let encrypted = try sut.encryptDataWithPublicKey(dataToEncrypt: anyString)
+                
+        try sut.deleteKeys()
+
+        let sut = KeyManagerService(configuration: .init(
+            id: testRunID.uuidString,
+            accessControlLevel: .open
+        ))
+
+        let error = #expect(throws: SecureStoreError.self) {
+            try sut.decryptDataWithPrivateKey(dataToDecrypt: encrypted)
+        }
+        
+        #expect(error?.kind == .cantDecryptData)
+        
+        let originalError = try #require(error?.originalError as? NSError)
+        #expect(originalError.code == errSecParam)
+        #expect(originalError.code == -50)
+    }
+    
+    /// This is a case where a as part of instantiating a `KeyManagerService`, a new set of keys is created
+    /// that is tied to the Secure Enclave (i.e. `kSecAttrTokenID: kSecAttrTokenIDSecureEnclave`).
+    ///
+    /// A SE key pair provides strong guarantees so that:
+    /// * It is never exported to a backup
+    /// * The key pair is deleted as soon as the device is erased
+    ///
+    /// In this case:
+    /// 1. a backup from an existing device, with previously encrypted data, was taken
+    /// 2. a restore on the device is performed
+    /// 3. the data is restored
+    /// 4. the key is not restored
+    ///
+    /// When attempting to **decrypt** the data, an `errSecParam` (aka -50) will be thrown
+    @Test("""
+            GIVEN a new `KeyManagerService` with a set of keys
+            AND a data is encrypted
+            AND the key is deleted due (e.g. due to a device erase)
+            AND a new `KeyManagerService` is created with the same (i.e. same `id`) configuration as part of a new app installation
+            WHEN attempting to decrypt the existing, previously backed-up, data
+            THEN throws `SecureStoreError.cantDecryptData`
+            AND the `originalError` is `errSecParam` (i.e. -50 OSStatus)
+    """)
+    func decryptKeyWithWrongPrivateKeyDueToOriginalKeyGoneMising() async throws {
+
+        let anyString = "any"
+        let encrypted = try sut.encryptDataWithPublicKey(dataToEncrypt: anyString)
+        
+        // Device Erased
+        let deleteQuery: [String: Any] = [
+            kSecClass as String: kSecClassKey,
+            kSecAttrApplicationTag as String: keyTag
+        ]
+        SecItemDelete(deleteQuery as CFDictionary)
+        
+        // New app installation
+        let sut = KeyManagerService(configuration: .init(
+            id: testRunID.uuidString,
+            accessControlLevel: .open
+        ))
+
+        let error = #expect(throws: SecureStoreError.self) {
+            try sut.decryptDataWithPrivateKey(dataToDecrypt: encrypted)
+        }
+        
+        #expect(error?.kind == .cantDecryptData)
+        
+        let originalError = try #require(error?.originalError as? NSError)
+        #expect(originalError.code == errSecParam)
+        #expect(originalError.code == -50)
     }
 }
