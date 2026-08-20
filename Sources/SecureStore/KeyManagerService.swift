a67e49733e81f83d5e4ad681075522f3bc63c6a7
Markos Zoulias Charatzas <markos.charatzas@dsit.gov.uk>
2026-08-20 10:29:08 +0100

    chore: PR feedback

--- Sources/SecureStore/KeyManagerService.swift
+++ Sources/SecureStore/KeyManagerService.swift
@@ -77,10 +77,8 @@ extension KeyManagerService {
     /// - Postcondition: A call to ``decryptDataWithPrivateKey(dataToDecrypt:)`` will throw a
     /// ``SecureStoreError(.cantRetrieveKey)`` error.
     func deleteKeys() throws {
-        let keyType = ["PublicKey", "PrivateKey", ""]
-        try keyType.forEach { key in
-            let keyName = configuration.id + key
-            let tag = keyName.data(using: .utf8)!
+        let keyTags = ["PublicKey", "PrivateKey", ""].map { configuration.id + $0 }.flatMap{ $0.data(using: .utf8) }
+        try keyTags.forEach { tag in
             let deleteQuery: [String: Any] = [kSecClass as String: kSecClassKey,
                                            kSecAttrApplicationTag as String: tag]
             
